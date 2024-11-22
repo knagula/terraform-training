141b499b0d558c36564cc41722b4c0cd2c88e512
knagula <knagula@akamai.com>
2024-11-21 23:37:57 -0800

    testing provider version init with operator

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    akamai = {
+      source  = "akamai/akamai"
+      version = "~> 6.1"
+    }
+  }
+  required_version = ">= 1.8"
+}
+ 
+provider "akamai" {
+  edgerc         = "~/.edgerc"
+  config_section = "default"
+  config {
+    
+  }
+}
